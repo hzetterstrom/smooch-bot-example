e4095d24563262fd37441454bd5d0ff190f6255a
hzetterstrom <hansz_2000@hotmail.com>
2016-09-01 17:35:09 -0400

    .

--- script.js
+++ script.js
@@ -177,4 +177,13 @@ menureturn: {
 			  
         
     },
+	
+	finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
+        }
+    }
 });
