ccf7bc16353b612652026facc97fe63babb0a86d
hzetterstrom <hansz_2000@hotmail.com>
2016-09-01 17:36:27 -0400

    .

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ menureturn: {
             return bot.getProp('name')
                 .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
                         'teach me how to do anything else!'))
-                .then(() => 'finish');
+                .then(() => 'menunew');
         }
     }
 });
