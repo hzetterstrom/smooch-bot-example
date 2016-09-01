b3fd1c6f49c3cc1a1fcf29e34098cd0302d4c104
hzetterstrom <hansz_2000@hotmail.com>
2016-09-01 17:30:26 -0400

    .

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Script = require('smooch-bot').Script;
+var NEWBIE = 0;
 
 module.exports = new Script({
     processing: {
@@ -10,9 +11,11 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
+		return bot.say('![](https://secure.gravatar.com/avatar/6df718bd56665a8d924fb58f3c23278b.jpg)')
+		.then(() => bot.say('Hi! I\'m Hans\' virtual assistant! Hans sends his regards and his apologies he couldn\'t be with us at the moment.'))
                 .then(() => 'askName');
-        }
+
+		}
     },
 
     askName: {
@@ -20,18 +23,158 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
-                .then(() => 'finish');
+                .then(() => 'menunew');
         }
     },
+	
+	
+menunew: {
 
-    finish: {
-        receive: (bot, message) => {
-            return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
-                .then(() => 'finish');
-        }
-    }
+				//prompt: (bot) => bot.say(`OK! ${name}.\n Let\'s get down to work. ` +
+				//`%[Import Hans\' contact info](http://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b.vcf) %[Hans\' Gravatar Profile](https://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b)`))
+				
+				 // `How can I assist? %[See Hans Linkedin Profile](reply:LinkedIn) %[Hans Resume](reply:Resume)`))
+		//return bot.getProp('name')
+		
+		// .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+          //              'teach me how to do anything else!'))
+		
+		
+				//prompt: (bot) => bot.say(`OK!\n Let\'s get down to work. ` +
+				
+				
+				
+		
+				prompt: (bot,name) => bot.say(`It\'s nice to meet you.\n Let me know what information you\'d like to see. ` +
+				`%[Contact Hans](reply:Contact) %[Random Hans Analogy](reply:Analogy) %[Hans\' Latest Tweets](reply:Tweets)`), 
+				
+				
+				receive: (bot, message) => {
+				var BOTMSG = message.text;
+				var LBOTMSG = BOTMSG.toLowerCase();
+				
+			
+			if (LBOTMSG == "contact") {
+				return bot.say(`%[Import Hans\' contact info](http://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b.vcf) %[Hans\' Gravatar Profile](https://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b)`)
+				//NEWBIE+=1
+				.then(() => 'menureturn');
+			} else if (LBOTMSG == "analogy") {
+				var RNDNUM = Math.floor(Math.random() * 11);
+				if (RNDNUM = 1) {
+					return bot.say('Random Analogy 1 ' + RNDNUM)
+					 .then(() => 'menureturn');
+				} else if (RNDNUM = 2) {
+					return bot.say('Random Analogy 2 ' + RNDNUM)
+					 .then(() => 'menureturn');
+				} else if (RNDNUM = 3) {
+					return bot.say('Random Analogy 3 ' + RNDNUM)
+					 .then(() => 'menureturn');
+				} 
+				//return bot.say('outside OK')
+			} else if (LBOTMSG == "tweets") {
+	
+					return bot.say("TWEET TWEET")
+				}
+			
+			
+
+
+			
+	//TWITTER
+	//OPEN CALENDAR
+	//Resume
+	//LAST ARTICLES
+	//ABOUT Hans
+		}
+			  
+        
+    },
+menureturn: {
+
+				//prompt: (bot) => bot.say(`OK! ${name}.\n Let\'s get down to work. ` +
+				//`%[Import Hans\' contact info](http://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b.vcf) %[Hans\' Gravatar Profile](https://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b)`))
+				
+				 // `How can I assist? %[See Hans Linkedin Profile](reply:LinkedIn) %[Hans Resume](reply:Resume)`))
+		//return bot.getProp('name')
+		
+		// .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+          //              'teach me how to do anything else!'))
+		
+		
+				//prompt: (bot) => bot.say(`OK!\n Let\'s get down to work. ` +
+		
+        //receive: (bot, message) => {
+        //    return bot.getProp('name')
+        //        .then((name) => bot.say(`Hello again, ${name}!`))
+        //        .then(() => 'finish');
+        //}
+		
+				prompt: (bot,name) => bot.say(`Welcome Back.\n Let me know what information you\'d like to see. ` +
+				`%[Contact Hans](reply:Contact) %[Random Hans Analogy](reply:Analogy)`), 
+				
+				
+				receive: (bot, message) => {
+				var BOTMSG = message.text;
+				var LBOTMSG = BOTMSG.toLowerCase();
+				
+			
+			if (LBOTMSG == "contact") {
+				return bot.say(`%[Import Hans\' contact info](http://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b.vcf) %[Hans\' Gravatar Profile](https://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b)`)
+				.then(() => 'menureturn');
+			} else if (LBOTMSG == "analogy") {
+			var RNDNUM = Math.floor((Math.random()*3) +1);
+				if (RNDNUM === 1) {
+					return bot.say('Random Analogy 1 ' + RNDNUM)
+					 .then(() => 'menureturn');
+				} else if (RNDNUM === 2) {
+					return bot.say('Random Analogy 2 ' + RNDNUM)
+					 .then(() => 'menureturn');
+				} else if (RNDNUM === 3) {
+					return bot.say('Random Analogy 3 ' + RNDNUM)
+					 .then(() => 'menureturn');
+				} else if (LBOTMSG == "twitter") {
+					var request = new XMLHttpRequest();
+					request.open("GET", "https://zapier.com/engine/rss/1617716/hanszed-tw1", false);
+					request.send();
+					var xml = request.responseXML;
+					var items = xml.getElementsByTagName("item");
+
+					for(var i = 0; i < items.length; i++) {
+					var item = items[i];
+  
+					var description = item.getElementsByTagName("description");
+					var links = item.getElementsByTagName("link");
+  
+				//	for(var k = 0; k < links.length; k++) {
+       //console.log(description[k].childNodes[0].nodeValue);
+					var description$i = description[i].childNodes[0].nodeValue;
+      //console.log(description$k)
+					//}
+					//for(var j = 0; j < links.length; j++) {
+      // console.log(links[j].childNodes[0].nodeValue);
+					var links$i = links[i].childNodes[0].nodeValue;
+					//}
+				}
+
+					return bot.say($links1 + $description1)
+					.then(() => bot.say($links2 + $description2))
+					.then(() => bot.say($links3 + $description3))
+					
+				}
+				return bot.say('outside OK')
+			}
+			
+			
+
+
+			
+	//TWITTER
+	//OPEN CALENDAR
+	//Resume
+	//LAST ARTICLES
+	//ABOUT Hans
+		}
+			  
+        
+    },
 });
